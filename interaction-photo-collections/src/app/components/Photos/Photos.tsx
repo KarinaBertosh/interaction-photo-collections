3d9d4e457fe6b47b1851bff7932023fc447b47ba
KarinaBertosh <cherrycake246@gmail.com>
2023-10-24 11:49:15 +0300

    feat: add pagination

--- interaction-photo-collections/src/app/components/Photos/Photos.tsx
+++ interaction-photo-collections/src/app/components/Photos/Photos.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { createApi } from "unsplash-js";
+import 'bootstrap/dist/css/bootstrap.css';
 import "./style.css";
 
 
@@ -10,6 +11,8 @@ const unsplash = createApi({ accessKey: `${CLIENT_ID}` });
 
 export default function Photos() {
   const [photos, setPhotos] = useState<any>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pages, setPages] = useState<number[]>([1, 2, 3]);
 
   useEffect(() => {
     unsplash.photos.list({}).then((result) => {
@@ -17,16 +20,61 @@ export default function Photos() {
     });
   }, []);
 
+  useEffect(() => {
+    unsplash.photos.list({ page: currentPage }).then((result) => {
+      setPhotos(result.response?.results);
+    });
+  }, [currentPage]);
+
+  const increasePage = () => {
+    if (currentPage === pages[pages.length - 1]) {
+      const newPagePagination = [...pages, pages[pages.length - 1] + 1];
+      newPagePagination.shift();
+      setPages(newPagePagination);
+    }
+  };
+
+  const reducePage = () => {
+    if (currentPage === pages[0] && currentPage !== 1) {
+      const newPagePagination = [...pages];
+      newPagePagination.pop();
+      newPagePagination.unshift(newPagePagination[0] - 1);
+      setPages(newPagePagination);
+    }
+  };
+
+  const handling = (value: boolean) => {
+    if (value) {
+      setCurrentPage(currentPage + 1);
+      increasePage();
+    } else if (!value && currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+      reducePage();
+    }
+  };
+
+
   return (
     <div className="photos">
-      {photos.map(({ urls: { regular } }: any) => (
-        <img
-          key={regular}
-          src={regular}
-          alt={regular}
-          className="photos__photo"
-        />
-      ))}
-    </div>
+      <div>
+        {photos.map(({ urls: { regular } }: any) => (
+          <img
+            key={regular}
+            src={regular}
+            alt={regular}
+            className="photos__photo"
+          />
+        ))}
+      </div>
+      <nav aria-label="Exam">
+        <ul className="pagination ">
+          <li className="page-item"><a className="page-link" href="#" onClick={() => handling(false)} >Last</a></li>
+          {pages.map((num: number) => (
+            <li key={num} className="page-item"><a className={`${currentPage === num ? "page-link active-page" : 'page-link'}`} href="#" onClick={() => setCurrentPage(num)}>{num}</a></li>
+          ))}
+          <li className="page-item"><a className="page-link" href="#" onClick={() => handling(true)} >Next</a></li>
+        </ul>
+      </nav>
+    </div >
   );
 }
